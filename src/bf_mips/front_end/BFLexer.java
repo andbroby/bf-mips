61a93df8d0556f55b0d53059cff54bdada1381a9
Andreas Broby <and.broby@gmail.com>
2015-03-19 02:30:15 +0100

    finish the lexer

--- src/bf_mips/front_end/BFLexer.java
+++ src/bf_mips/front_end/BFLexer.java
@@ -4,20 +4,36 @@ import bf_mips.BFSyntaxError;
 
 import java.util.HashSet;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class BFLexer {
-    public static char[] analyze(String code) throws BFSyntaxError {
+    public static char[] lex(String code) {
         Character[] keywords_array = new Character[] {'<', '>', '+', '-', '.', ',', '[', ']'};
         HashSet<Character> keywords = new HashSet<Character>(
                 Arrays.asList(keywords_array)
         );
 
         char[] lexemes = code.replace(" ", "").toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+
 
         for (Character lexeme : lexemes) {
             if (! keywords.contains(lexeme)) {
                 throw new BFSyntaxError(lexeme);
             }
+
+            if (lexeme == '[') {
+                stack.push(lexeme);
+            } else if (lexeme == ']') {
+                if (stack.isEmpty()) {
+                    throw new BFSyntaxError(lexeme);
+                }
+                stack.pop();
+            }
+        }
+
+        if (! stack.isEmpty()) {
+            throw new BFSyntaxError(']');
         }
 
         return lexemes;
