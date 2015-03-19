0967aeed08ef36af03014de10a73d43c931ba316
Andreas Broby <and.broby@gmail.com>
2015-03-19 02:31:09 +0100

    wrote the parser

--- src/bf_mips/front_end/BFParser.java
+++ src/bf_mips/front_end/BFParser.java
@@ -4,6 +4,12 @@ import bf_mips.structs.AST;
 
 public class BFParser {
     public static AST parse(char[] tokens) {
-        return new AST();
+        AST tree = new AST();
+
+        for (int i = 0; i < tokens.length; i++) {
+            tree.add_node(tokens[i]);
+        }
+
+        return tree;
     }
 }
