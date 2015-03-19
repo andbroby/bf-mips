2c03d8d7ac20ad29fd836a34aa545e282e61b9fa
Andreas Broby <and.broby@gmail.com>
2015-03-19 02:29:46 +0100

    add syntax error exception

--- src/bf_mips/BFSyntaxError.java
+++ src/bf_mips/BFSyntaxError.java
@@ -3,7 +3,7 @@ package bf_mips;
 /**
  * Created by broby on 17/03/15.
  */
-public class BFSyntaxError extends Exception {
+public class BFSyntaxError extends Error {
     public BFSyntaxError(Character unrecognizedCharacter) {
         super(String.format("Unrecognized token: %s", unrecognizedCharacter));
     }
